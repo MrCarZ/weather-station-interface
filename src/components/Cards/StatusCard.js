74269edfcbb03ac1a73286428adf83b6d76843af
Carz Eduardo <eduardovinagre123@gmail.com>
2021-11-17 12:26:32 -0300

    Finish StatusCard Prototype

--- src/components/Cards/StatusCard.js
+++ src/components/Cards/StatusCard.js
@@ -1,18 +1,66 @@
-import React from 'react';
-import StatusInfo from './StatusInfo';
-import { Card, CardContent } from '@mui/material';
-import {Thermometer} from 'react-feather';
+import React from "react";
+import StatusInfo from "./StatusInfo";
+import { Card, CardContent, Typography } from "@mui/material";
+import { Thermometer, CloudDrizzle, Sun, CloudLightning } from "react-feather";
+
+const data = [
+  {
+    name: "Temperature",
+    Icon: () => <Sun size={25} />,
+    info: {
+      "Last Measure": 0,
+      "Max (Last 24h)": 23,
+      "Min (Last 24h)": 22,
+    },
+  },
+  {
+    name: "Humidity",
+    Icon: () => <CloudDrizzle size={25} />,
+    info: {
+      "Last Measure": 0,
+      "Max (Last 24h)": 25,
+      "Min (Last 24h)": 26,
+    },
+  },
+  {
+    name: "Pressure",
+    Icon: () => <Thermometer size={25} />,
+    info: {
+      "Last Measure": 0,
+      "Max (Last 24h)": 23,
+      "Min (Last 24h)": 22,
+    },
+  },
+  {
+    name: "Last Lightning Strike",
+    Icon: () => <CloudLightning size={25} />,
+    info: {
+      Day: new Date().toDateString(),
+      Second: 23,
+      Millisecond: 22,
+    },
+  }
+];
 
 const StatusCard = (props) => {
-    const {data} = props;
-    
-    return(
-        <Card sx={{ width: 250, height: 250 }}>
-            <CardContent>
-                <StatusInfo name="Temperature" ></StatusInfo>                
-            </CardContent>
-        </Card>
-    )
-}
+  return (
+    <Card sx={{ width: 550, height: 600, overflow: "auto" }}>
+      <Typography
+        gutterBottom
+        component="div"
+        sx={{ marginTop: 3, fontSize: 20, fontWeight: "bolder" }}
+        variant="button"
+        align="center"
+      >
+        General Information
+      </Typography>
+      <CardContent>
+        {data?.map((e) => (
+          <StatusInfo name={e.name} Icon={e.Icon} info={e.info} />
+        ))}
+      </CardContent>
+    </Card>
+  );
+};
 
 export default StatusCard;
--- src/components/Cards/StatusInfo.js
+++ src/components/Cards/StatusInfo.js
@@ -1,48 +1,48 @@
 import React from "react";
-import {Typography} from '@mui/material';
-
-
+import { List, ListItem, ListItemText, ListItemIcon } from "@mui/material";
 
 const StatusInfo = (props) => {
-    const {name, last, min, max} = props;
-    return (
-    <>
-      <Typography gutterBottom variant="overline" align="left" component="div">
-        {name} 
-      </Typography>
+  const { name, info, Icon } = props;
+  const keys = Object.keys(info);
 
-      <Typography
-        gutterBottom
-        variant="body2"
-        align="left"
-        component="p"
-        color="text.secondary"
-      >
-        Last Measure:
-        
-        <span textAlign="right">0</span>
-        
-      </Typography>
-      <Typography
-        gutterBottom
-        variant="body2"
-        align="left"
-        component="p"
-        color="text.secondary"
-      >
-        {" "}
-        Minimum (Last 24h): 0
-      </Typography>
-      <Typography
-        gutterBottom
-        variant="body2"
-        align="left"
-        component="p"
-        color="text.secondary"
-      >
-        {" "}
-        Maximum (Last 24h): 0
-      </Typography>
+  return (
+    <>
+      <List>
+        <ListItem key={`${name}-header`} secondaryAction={<Icon />}>
+          <ListItemText
+            primary={name}
+            primaryTypographyProps={{
+              variant: "button",
+              fontSize: "15px",
+              fontWeight: "bolder",
+            }}
+          />
+        </ListItem>
+        {keys.map((key) => {
+          return (
+            <ListItem
+              key={`${name}-${key}`}
+              secondaryAction={
+                <ListItemText
+                  primary={info[key]}
+                  primaryTypographyProps={{
+                    variant: "overline",
+                    fontSize: "13px",
+                    fontWeight: "medium",
+                  }}
+                />
+              }
+            >
+              <ListItemText
+                primary={key}
+                primaryTypographyProps={{
+                  variant: "overline",
+                }}
+              />
+            </ListItem>
+          );
+        })}
+      </List>
     </>
   );
 };
