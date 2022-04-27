35ad73956dee819b6eb7ed57b0a345278e16e9a3
Carz Dias <eduardovinagre123@gmail.com>
2022-04-26 23:55:25 -0300

    Update MultiChart.js with new imported functions

--- src/components/MultiChart.js
+++ src/components/MultiChart.js
@@ -1,53 +1,97 @@
-import React, { useState, useContext, useEffect, useRef } from "react";
+import React, { useState, useContext, useEffect, useRef, useCallback } from "react";
 import config from "../chart/config";
 import { Line } from "react-chartjs-2";
 import { WeatherStationContext } from "../contexts/weatherStationContext";
-import socketio from "socket.io-client";
-import createMeasureObject from "../helpers/createMeasureObject";
+import { mapDataToArrayPosition } from "../helpers/mapDataToArrayPosition";
+import { checkValidLocalStorage } from "../helpers/checkValidLocalStorage";
+import { socketService } from "../services/socketService";
+import { weatherStationService } from "../services/weatherStationService";
 
 const MultiChart = () => {
   const chartRef = useRef(null);
-  const socket = socketio.connect("http://192.168.0.12:3000/");
-  const { weatherData, appendWeatherData } = useContext(WeatherStationContext);
+
+  const { weatherData, insertWeatherData, last } = useContext(WeatherStationContext);
 
   const labels = weatherData.timestamp;
+
   const validDatasets = Object.keys(weatherData).filter((key) => {
     if (key !== "timestamp") {
       return weatherData[key];
     }
   });
+
   const datasets = validDatasets.map((value) => weatherData[value]);
-  console.log(datasets)
 
   const [chartData, setChartData] = useState({
     labels: labels,
     datasets: datasets,
   });
 
-  useEffect(() => {
-    socket.on("rasp-message", (newMeasure) => {
-      //TODO: Recreate this method in back-end
-      const measureObject = createMeasureObject(JSON.parse(newMeasure));
-      console.log(measureObject);
-      updateChartData(measureObject);
-    });
+  const getInitialWeatherData = useCallback(async () => {
+    try {
+      /* Check for LocalStorage Data, if updated, then don't fetch from API */
+      if (checkValidLocalStorage("weatherStationApp") === false) {
+        const currentDate = new Date();
+        const weatherDataFromAPI = await weatherStationService.getDataFromAPI(currentDate);
+        updateChartData(weatherDataFromAPI);
+      }
+    }
+    catch (error) {
+      console.log(error);
+    }
+    finally {
+      /* Either Way, after fetching or just retrieving data from LocalStorage, initialize socket */
+      socketService.socketOn("rasp-message", (newMeasure) => {
+        const newMeasureArrayObject = JSON.parse(newMeasure);
+        updateChartData(newMeasureArrayObject);
+      });
+    }
   }, []);
 
-  const updateChartData = (lastMeasure) => {
-    const chart = chartRef.current;
+  useEffect(() => {
+    /* Mounting Component */
+    getInitialWeatherData();
 
+    /* Unmounting Component */
+    return () => socketService.socketDisconnect();
+  }, [getInitialWeatherData]);
+
+  const updateChartData = (newMeasureArrayObject) => {
     const { labels, datasets } = chartData;
 
-    labels?.push(lastMeasure.timestamp);
 
-    datasets[0].data.push(lastMeasure.temperature);
-    datasets[1].data.push(lastMeasure.pressure);
-    datasets[2].data.push(lastMeasure.humidity);
-    datasets[3].data.push(lastMeasure.lightningStrike);
+    newMeasureArrayObject.map((newMeasureObject) => {
+
+      const dataIndex = mapDataToArrayPosition(newMeasureObject.timestamp, newMeasureObject.lightningStrike);
+      if (!newMeasureObject.lightningStrike) {
+        const timestampAsDate = new Date(newMeasureObject.timestamp);
+        labels[dataIndex] = timestampAsDate.toLocaleTimeString("pt-br");
+      }
+
+      validDatasets.map((dataType, index) => {
+
+        if (dataType === "lightningStrike") {
+          if (newMeasureObject.lightningStrike) {
+            console.log("raio");
+            console.log(datasets[index][dataIndex]);
+            const newdata = datasets[index][dataIndex]+1; 
+            datasets[index][dataIndex] =
+              (datasets[index].data[dataIndex] === null) ? 1 : newdata;
+
+            console.log(datasets[index][dataIndex]);
+          }
+        }
+        else {
+          datasets[index].data[dataIndex] = newMeasureObject[dataType];
+        }
+      });
+    })
+
+    insertWeatherData(newMeasureArrayObject);
+    const chart = chartRef.current;
 
     if (chart) {
       chart.update();
-      appendWeatherData(lastMeasure);
     }
   };
 
